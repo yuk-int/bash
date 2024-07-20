5c1616a14aa78675cc58b41db0d32af9dc67b108
yuken-int <yuken.int@gmail.com>
2024-07-20 15:47:33 +0800

    2024/7/20 update

--- ipSearch.sh
+++ ipSearch.sh
@@ -12,9 +12,9 @@ ifconfig | grep inet | grep -v '127.0.0.1' | cut -d: -f2 | awk '{print $2}'
 read -p "subnet:" subnet
 
 if [ -z "$subnet" ]; then
-echo "-z is true"
+echo "-z $subnet is true"
 
 else
-echo "searching"
+echo "searching $subnet .1 to .254"
 for ip in $(seq 1 254); do ping -c 1 -W 1 $subnet.$ip | grep "ttl"; done
 fi
