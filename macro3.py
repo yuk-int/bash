1d0d2345302c8a31ac6852b22dbb000ee41b404f
yuken-int <yuken.int@gmail.com>
2024-08-17 14:55:54 +0800

    update_20240817

--- macro3.py
+++ macro3.py
@@ -1,42 +1,23 @@
-import signal
-import RPi.GPIO as GPIO
-from gpiozero import Button
-from gpiozero import LED
-from datetime import datetime
-from time import sleep
+#macro3.py to receive mqtt data from macro4.py
+#macro4.py
+#breadbaord with LED light and button
+#use button to toggle LED light
 
-def initialization():
-    button = Button(pin=18)
-    red = LED(pin=25)
-    print ("initialization is done!")
-    return button, red
+import paho.mqtt.client as mqtt
+#from paho.mqtt import client as client
+import redis
 
-def twinkle():
-    #red.blink()
-    print("hello")
+redis_conn = redis.Redis(host='localhost', port=6379, password='redispass')
 
-def action():
-    red.toggle()
-    print("botton is toggled")
-    now = datetime.now()
-    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    if red.is_lit:
-        print(f"light is ON_{now_str}")
-    else:
-        print(f"light is OFF_{now_str}")
-    
-'''
-while True:
-    if button.is_pressed:
-        print("Button is pressed")
-        red.on()
-    else:
-        print("Button is not pressed")
-'''
+def on_message(mosq, obj, msg):
+    topic = msg.topic
+    message = msg.payload.decode('utf-8')
+    print (f"topic={topic}, message={message}")
 
 if __name__ == '__main__':
-    button, red = initialization()
-    button.when_released = action
-    #button.when_released = twinkle
-    red.on()
-    signal.pause()
+    
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.on_message = on_message
+    client.connect('127.0.0.1')
+    client.subscribe('btn/func1', qos=2)
+    client.loop_forever()
---
+++ macro4.1.py
@@ -0,0 +1,52 @@
+#breadbaord with LED light and button
+#use button to toggle LED light
+
+import signal
+import RPi.GPIO as GPIO
+from gpiozero import Button
+from gpiozero import LED
+from datetime import datetime
+from time import sleep
+import paho.mqtt.publish as publish
+
+def initialization():
+    button = Button(pin=18)
+    red = LED(pin=25)
+    print ("initialization is done!")
+    return button, red
+
+def twinkle():
+    #red.blink()
+    print("hello")
+
+def action():
+    red.toggle()
+    print("botton is toggled")
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    if red.is_lit:
+        print(f"light is ON_{now_str}")
+        publish.single(topic="btn/func1", payload="test1", hostname="localhost", qos=2)
+        #print ("mqtt-publish1")
+    else:
+        print(f"light is OFF_{now_str}")
+        publish.single(topic="btn/v", payload="test2", hostname="localhost", qos=2)
+        #print ("mqtt-publish2")
+
+
+'''
+while True:
+    if button.is_pressed:
+        print("Button is pressed")
+        red.on()
+    else:
+        print("Button is not pressed")
+'''
+
+if __name__ == '__main__':
+    button, red = initialization()
+    button.when_released = action
+    #button.when_released = twinkle
+    #red.on()
+    signal.pause()
