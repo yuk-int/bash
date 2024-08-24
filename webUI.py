0b18e9c842e7b79b55c7be532cd95f5d8452ed20
yuken-int <yuken.int@gmail.com>
2024-08-24 14:43:44 +0800

    update

--- webUI.py
+++ webUI.py
@@ -3,6 +3,8 @@ import redis
 from dotenv import load_dotenv
 import os
 import json
+import pandas as pd
+from streamlit_autorefresh import st_autorefresh
 
 load_dotenv()
 
@@ -15,5 +17,13 @@ b_list = redis_conn.lrange('btn/func1',-10,-1)
 #byte list to str
 str_list = [byte.decode('utf-8') for byte in b_list]
 dict = [json.loads(string) for string in str_list]
-st.write(dict)
+#st.write(dict)
+df = pd.DataFrame(dict)
 
+
+st.dataframe(df,
+             hide_index=True,
+             column_config={
+                 "Status":st.column_config.CheckboxColumn(label="botton status", width="small"),
+                 "Date":st.column_config.DatetimeColumn(label="time", width="small")
+             })
